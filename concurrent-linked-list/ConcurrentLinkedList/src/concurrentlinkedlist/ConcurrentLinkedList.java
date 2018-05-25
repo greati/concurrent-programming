f8a18c1ebc9db3afa7cc8f8c4d19a80da12aafa2
Carlos Vieira <vieiramecarlos@gmail.com>
2018-05-24 22:22:35 -0300

    locked operations

--- concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedList.java
+++ concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedList.java
@@ -6,6 +6,8 @@
 package concurrentlinkedlist;
 import java.util.NoSuchElementException;
 import java.lang.IllegalArgumentException;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
@@ -16,10 +18,17 @@ public class ConcurrentLinkedList {
     private Node tail;
     private int length;
     
+    private final ReentrantReadWriteLock searchRemoveLock;
+    private final ReentrantLock insertRemoveLock;
+    
     public ConcurrentLinkedList() {
         this.head = null;
         this.tail = null;
         this.length = 0;
+        
+        boolean fair = true;
+        this.searchRemoveLock = new ReentrantReadWriteLock(fair);
+        this.insertRemoveLock = new ReentrantLock(fair);
     }
 
     public Node getHead() {
@@ -34,106 +43,167 @@ public class ConcurrentLinkedList {
         return length;
     }
     
-    // search and remove
-    public void removeNode(Node n) {
-        int pos = 0;
-        Node prev = null;
-        Node curr = this.head;
-        while(curr != null) {
-            if (curr == n) {
-                this.remove(prev, curr);
-                return;
+    @Override
+    public String toString() {
+        this.searchRemoveLock.readLock().lock();
+        try {
+            String list = "[";
+            Node n = this.head;
+            while(n != null) {
+                list += Integer.toString(n.getData());
+                n = n.getNext();
+                if (n != null)
+                    list += ", ";
             }
-            
-            prev = curr;
-            curr = curr.getNext();
+            list += "]";
+
+            return list;
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
         }
-        throw new NoSuchElementException();
+    }
+    
+    // search and remove
+    public void remove(Node n) {
+        Node prev = this.getPrevious(n);
+        this.remove(prev, n);
     }
     
     // search and remove
     public void removeAt(int pos) {
-        if (pos < 0 || pos >= this.length)
-            throw new IllegalArgumentException();
-        
-        Node prev = null;
-        Node curr = this.head;
-        for (int i = 0; i < pos; ++i) {
-            prev = curr;
-            curr = curr.getNext();
+        if (pos > 0) {
+            Node prev = this.getNodeAt(pos-1);
+            this.remove(prev, prev.getNext());
+        } else {
+            this.remove(null, this.head);
         }
-        
-        this.remove(prev, curr);
     }
     
     // search and remove
     public void removeValue(int data) {
-        int pos = 0;
-        Node prev = null;
-        Node curr = this.head;
-        while(curr != null) {
-            if (curr.getData() == data) {
-                this.remove(prev, curr);
-                return;
-            }
-            
-            prev = curr;
-            curr = curr.getNext();
+        this.searchRemoveLock.readLock().lock();
+        Node n, prev;
+        try {
+            n = this.search(data);
+            prev = this.getPrevious(n);
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
         }
-        throw new NoSuchElementException();
+        this.remove(prev, n);
     }
     
     // remove
     private void remove(Node prev, Node target) {
-        if (target == this.head)
-            this.head = target.getNext();
-        else
-            prev.setNext(target.getNext());
-        if (target == this.tail)
-            this.tail = prev;
-        this.length--;
+        this.searchRemoveLock.writeLock().lock();
+        this.insertRemoveLock.lock();
+        try {
+            if (target == this.head)
+                this.head = target.getNext();
+            else
+                prev.setNext(target.getNext());
+            if (target == this.tail)
+                this.tail = prev;
+            this.length--;
+        } finally {
+            this.insertRemoveLock.unlock();
+            this.searchRemoveLock.writeLock().unlock();
+        }
     }
     
-    // Insert
+    // insert
     public void insert(int data) {
         Node n = new Node(data);
         this.insert(n);
     }
     
-    // Insert
+    // insert
     public void insert(Node n) {
-        if (this.length > 0)
-            this.tail.setNext(n);
-        else
-            this.head = n;
-        
-        this.tail = n;
-        this.length++;
+        this.insertRemoveLock.lock();
+        try {
+            if (this.length > 0)
+                this.tail.setNext(n);
+            else
+                this.head = n;
+
+            this.tail = n;
+            this.length++;
+        } finally {
+            this.insertRemoveLock.unlock();  
+        }
     }
     
     // search
     public Node search(int data) {
-        int pos = 0;
-        Node i = this.head;
-        while(i != null) {
-            if (i.getData() == data)
-                return i;
-            i = i.getNext();
-            pos++;
+        this.searchRemoveLock.readLock().lock();
+        try {
+            int pos = 0;
+            Node i = this.head;
+            while(i != null) {
+                if (i.getData() == data)
+                    return i;
+                i = i.getNext();
+                pos++;
+            }
+            return null;
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
         }
-        return null;
+    }
+    
+    // search
+    public Node getPrevious(Node n) {
+        this.searchRemoveLock.readLock().lock();
+        try {
+            int pos = 0;
+            Node prev = null;
+            Node curr = this.head;
+            while(curr != null) {
+                if (curr == n) {
+                    return prev;
+                }
+
+                prev = curr;
+                curr = curr.getNext();
+            }
+            throw new NoSuchElementException();
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
+        }
+    }
+    
+    // search
+    public Node getNodeAt(int pos) {
+        this.searchRemoveLock.readLock().lock();
+        try {
+            if (pos < 0 || pos >= this.length)
+                throw new IllegalArgumentException();
+
+            Node curr = this.head;
+            for (int i = 0; i < pos; ++i) {
+                curr = curr.getNext();
+            }
+
+            return curr;
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
+        }
     }
     
     // search
     public int getPosition(Node n) {
-        int pos = 0;
-        Node i = this.head;
-        while(i != null) {
-            if (i == n)
-                return pos;
-            i = i.getNext();
-            pos++;
+        this.searchRemoveLock.readLock().lock();
+        try {
+            int pos = 0;
+            Node i = this.head;
+            while(i != null) {
+                if (i == n)
+                    return pos;
+                i = i.getNext();
+                pos++;
+            }
+            throw new NoSuchElementException();
+        } finally {
+            this.searchRemoveLock.readLock().unlock();
         }
-        throw new NoSuchElementException();
     }
 }
--- concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedListDriver.java
+++ concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedListDriver.java
@@ -16,6 +16,24 @@ public class ConcurrentLinkedListDriver {
      */
     public static void main(String[] args) {
         ConcurrentLinkedList l1 = new ConcurrentLinkedList();
-        
+        Node n = new Node(33);
+        l1.insert(10);
+        l1.insert(5);
+        l1.insert(n);
+        l1.insert(7);
+        System.out.println(l1.getPosition(n));
+        System.out.println(l1.getPosition(l1.search(5)));
+        System.out.println(l1);
+        l1.remove(n);
+        System.out.println(l1);
+        l1.removeAt(1);
+        System.out.println(l1);
+        System.out.println(l1.getLength());
+        l1.insert(-99);
+        System.out.println(l1);
+        l1.remove(l1.getHead());
+        System.out.println(l1);
+        l1.remove(l1.getTail());
+        System.out.println(l1);
     }
 }
