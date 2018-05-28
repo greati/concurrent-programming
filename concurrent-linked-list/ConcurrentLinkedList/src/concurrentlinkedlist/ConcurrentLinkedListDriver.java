73fef8009193323685f6f09a00bff3bc3536d424
Carlos Vieira <vieiramecarlos@gmail.com>
2018-05-28 09:18:29 -0300

    test operation subsets

--- concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedListDriver.java
+++ concurrent-linked-list/ConcurrentLinkedList/src/concurrentlinkedlist/ConcurrentLinkedListDriver.java
@@ -5,6 +5,7 @@
  */
 package concurrentlinkedlist;
 
+import java.util.ArrayList;
 import concurrentlinkedlist.ListUser.Operation;
 import java.util.Random;
 
@@ -13,39 +14,42 @@ import java.util.Random;
  * @author carlosv
  */
 public class ConcurrentLinkedListDriver {
-    private static final int OPERATIONS = 100;
+    private static ConcurrentLinkedList list;
     public static final Random RANDOM_GEN = new Random();
     private static final int MAX_VALUE = 3;
+    
+    public static Operation randomOperation(boolean search, boolean insert,
+            boolean remove) {
+        ArrayList<Operation> options = new ArrayList<>();
+        int n = 0;
+        if (search) options.add(Operation.SEARCH);
+        if (insert) options.add(Operation.INSERTION);
+        if (remove) options.add(Operation.REMOVAL);
+        
+        int op_num = RANDOM_GEN.nextInt(options.size());
+        return options.get(op_num);
+    }
+    
+    public static void test(boolean search, boolean insert, boolean remove,
+            int operations) {
+        for (int i = 0; i < operations; ++i) {
+            int value = RANDOM_GEN.nextInt(2*MAX_VALUE+1) - MAX_VALUE;
+            
+            Operation op = randomOperation(search, insert, remove);
+            ListUser user = new ListUser(list, op, value);
+            user.start();
+        }
+    }
    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ConcurrentLinkedList list = new ConcurrentLinkedList();
+        list = new ConcurrentLinkedList();
         for (int i = -MAX_VALUE; i <= MAX_VALUE; ++i) {
             list.insert(i, true);
         }
         System.out.println(list);
-        
-        for (int i = 0; i < OPERATIONS; ++i) {
-            int value = RANDOM_GEN.nextInt(2*MAX_VALUE+1) - MAX_VALUE;
-            
-            int op_num = RANDOM_GEN.nextInt(3);
-            Operation op;
-            switch(op_num) {
-                case 1:
-                    op = Operation.INSERTION;
-                    break;
-                case 2:
-                    op = Operation.REMOVAL;
-                    break;
-                default:
-                    op = Operation.SEARCH;
-                    break;
-            }
-            ListUser user = new ListUser(list, op, value);
-            user.start();
-        }
-        
+        test(true, true, false, 10);
     }
 }
